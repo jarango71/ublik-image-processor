3b00443eda6a1ff4a02e6bf0fd7259221e03a94e
jarango71 <jarango71@gmail.com>
2023-10-03 11:31:38 -0500

    delete

--- src/main/java/net/ublik/image/processor/ImageProcessor.java
+++
@@ -1,8 +0,0 @@
-package net.ublik.image.processor;
-
-import java.io.IOException;
-
-public interface ImageProcessor {
-
-	byte[] addTextWatermark(String url, String text, String type, int fontSize,  int positionX, int positionY, String color, float alpha) throws IOException;
-}
